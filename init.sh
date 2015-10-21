4eb42152544700ce36243c83e8f26ba265f6026b
Mark Fine <mark.fine@gmail.com>
2015-10-20 22:08:55 -0700

    More init stuff.

--- init.sh
+++ init.sh
@@ -4,14 +4,14 @@ set -e
 
 name=${PWD##*/}
 
-rm -rf .git/
+rm -rf init.sh .git/
 
 for f in template.cabal README.md Dockerfile; do
   perl -i -pe "s/template/$name/g" "$f"
 done
 
 mv template.cabal "$name.cabal"
-rm -f init.sh
 
+git init
 git add .
 git commit -m "Initial import from template."
